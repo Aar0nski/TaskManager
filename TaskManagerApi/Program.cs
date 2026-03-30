1e1c7146598ba95590df986c96818ed00269ccc2
aar0nski <aaron.enander04@gmail.com>
2026-03-30 15:06:50 +0200

    added cs files and database

--- TaskManagerApi/Program.cs
+++ TaskManagerApi/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Data;
+using TaskManagerApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITaskService, TaskService>();
 
 var app = builder.Build();
 
@@ -12,5 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
